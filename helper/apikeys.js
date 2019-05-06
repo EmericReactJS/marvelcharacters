40d6052ee6d0e12e1b19fed7a3b864f354fddbad
emeric <emeric555@gmail.com>
2019-05-06 15:36:31 +0200

    helper

--- helper/apikeys.js
+++
@@ -1,9 +0,0 @@
-const apiKey = 'a1554b62fda6375c4b6ef43f7cb4cfe9';
-const timeStamp = '1';
-const hashPass = '5e7e2a348a47510cee643bf096d44061';
-
-module.exports = {
-  apiKey,
-  timeStamp,
-  hashPass
-}
