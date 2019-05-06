ab9d5a373ea374be5939d038b73200a41d219271
emeric <emeric555@gmail.com>
2019-05-06 15:31:26 +0200

    heroku

---
+++ helper/apikeys.js
@@ -0,0 +1,9 @@
+const apiKey = 'a1554b62fda6375c4b6ef43f7cb4cfe9';
+const timeStamp = '1';
+const hashPass = '5e7e2a348a47510cee643bf096d44061';
+
+module.exports = {
+  apiKey,
+  timeStamp,
+  hashPass
+}
